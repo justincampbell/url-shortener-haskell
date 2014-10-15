751c2e241878f2b9d7342c81378a00f74fbb4e03
Justin Campbell <justin@justincampbell.me>
2014-10-15 09:46:10 -0400

    Return new state

--- Shortener.hs
+++ Shortener.hs
@@ -4,8 +4,10 @@ type Url = String
 
 data State = State { _id :: Int }
 
-shorten :: State -> Url -> String
-shorten state _ = show $ inc (_id state)
+shorten :: State -> Url -> (State, String)
+shorten state _ = (newstate, token) where
+    newstate = State $ inc (_id state)
+    token = show (_id newstate)
 
 inc :: Int -> Int
 inc n = n + 1
--- Spec.hs
+++ Spec.hs
@@ -7,6 +7,6 @@ main :: IO ()
 main = hspec $
         describe "shorten" $
             it "returns the first id" $
-                shorten s url `shouldBe` "1"
+                snd (shorten s url) `shouldBe` "1"
             where s = State 0
                   url = "foo"
