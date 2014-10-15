7277b3eb48733e53ed22ed3881828a7a8d7d767b
Justin Campbell <justin@justincampbell.me>
2014-10-15 09:56:50 -0400

    Create Token type

--- Shortener.hs
+++ Shortener.hs
@@ -1,10 +1,11 @@
 module Shortener where
 
+type Token = String
 type Url = String
 
 data State = State { _id :: Int }
 
-shorten :: State -> Url -> (State, String)
+shorten :: State -> Url -> (State, Token)
 shorten state _ = (newstate, token) where
     newstate = State $ succ (_id state)
     token = show (_id newstate)
