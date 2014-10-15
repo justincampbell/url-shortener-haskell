a3c8ed8a92da5ed473322a8ebe147c5120c3eb6f
Justin Campbell <justin@justincampbell.me>
2014-10-15 09:55:14 -0400

    Add test for 2 increments

--- Spec.hs
+++ Spec.hs
@@ -5,8 +5,12 @@ import Test.Hspec
 
 main :: IO ()
 main = hspec $
-        describe "shorten" $
-            it "returns the first id" $
-                snd (shorten s url) `shouldBe` "1"
-            where s = State 0
+        describe "shorten" $ do
+            it "returns the first token" $
+                snd (shorten emptystate url) `shouldBe` "1"
+            it "increments every token" $
+                let (newstate, token) = shorten emptystate url in do
+                    token `shouldBe` "1"
+                    snd (shorten newstate url) `shouldBe` "2"
+            where emptystate = State 0
                   url = "foo"
